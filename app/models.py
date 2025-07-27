da0b65047b07cb436824d586f6923c2ceb7a6f61
Radee <radeehasadream@gmail.com>
2025-07-27 21:14:33 +0600

    Added applied jobs model

--- app/models.py
+++ app/models.py
@@ -5,3 +5,22 @@ class Jobs:
 	__tablename__ = "jobs"
 
 	id = Column(Integer, primary_key=True, index=True)
+	title = Column(String, index=True, nullable=False)
+	min_salary = Column(Integer)
+	max_salary = Column(Integer)
+	location = Column(String, nullable=False)
+	employer_name = Column(String, nullable=False)
+
+class Users:
+	__tablename__ = "users"
+
+	id = Column(Integer, primary_key=True, index=True)
+	username = Column(String, index=True, nullable=False)
+	password = Column(String, nullable=False)
+
+class AppliedJobs:
+	__tablename__ = "appliedjobs"
+
+	id = Column(Integer, primary_key=True, index=True)
+	employee_id = Column(Integer, ForeignKey("users.id"))
+	job_id = Column(Integer, ForeignKey("jobs.id"))
--- app/scraper/scraper.py
+++ app/scraper/scraper.py
@@ -6,9 +6,12 @@ import os
 
 dotenv_path = Path(__file__).parents[1] / ".env"
 load_dotenv(dotenv_path=dotenv_path)
+
 APIKEY = os.getenv("APIKEY")
 url = os.getenv("JOBSURL")
+
 auth = HTTPBasicAuth(APIKEY,"")
 
-def jobs_run_scraper():
-	requests.get()
+def jobs_run_scraper(params: dict = None):
+	response = requests.get(url, params=params, auth=auth)
+	return response
